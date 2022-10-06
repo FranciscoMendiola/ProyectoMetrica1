a713181ac4cc62fead8a176cb8af2ef2fab8a821
FranciscoMendiola <franciscomendiola9@ciencias.unam.mx>
2022-10-05 22:31:26 -0500

    Tercera versión

--- src/Metricas1/OperacionesConPuntos2D.java
+++ src/Metricas1/OperacionesConPuntos2D.java
@@ -19,17 +19,25 @@ package Metricas1;
 public class OperacionesConPuntos2D {
 
 
-    public static double normaEuclideana(Punto2D p) { return 0; }
+    public static double normaEuclideana(Punto2D p) {
+        double norma = metricaEuclidiana(new Punto2D(), p);
+        return norma;
+    }
 
+    public static double metricaEuclidiana(Punto2D a, Punto2D b) {
+        double distanciaAB = Math.sqrt(Math.pow( (a.getX() - b.getX()) , 2) + Math.pow( (a.getY() - b.getY()) , 2));
+        return distanciaAB;
+    }
 
-    public static double metricaEuclidiana(Punto2D a, Punto2D b) { return 0;}
+    public static double metricaTaxista(Punto2D a, Punto2D b) {
+        double distanciaT = Math.abs(a.getX() - b.getX()) + Math.abs(a.getY() - b.getY());
+        return distanciaT; }
 
+    public static double dotProduct(Punto2D a, Punto2D b) {
+        double producto = (a.getX()*b.getX()) + (a.getY()*b.getY());
+        return producto; }
 
-    public static double metricaTaxista(Punto2D a, Punto2D b) { return 0; }
-
-
-    public static double dotProduct(Punto2D a, Punto2D b) { return 0; }
-
-
-    public double areaPalalelogramo(Punto2D a, Punto2D b) { return 0; }
+    public double areaPalalelogramo(Punto2D a, Punto2D b) {
+        double area = Math.abs(a.getX() * a.getY()) / 2;
+        return area; }
 }
--- src/Metricas1/UsaOperacionesConPuntos2D.java
+++ src/Metricas1/UsaOperacionesConPuntos2D.java
@@ -10,24 +10,35 @@
  */
 
 package Metricas1;
-
+import javax.swing.*;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class UsaOperacionesConPuntos2D {
 
-    public static void main(String... something){
-        Punto2D a = new Punto2D(4,3);
+    public static void main(String... something) {
+
+        double ax = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor de x para el punto a"));
+        double ay = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor de y para el punto a"));
+
+
+        Punto2D a = new Punto2D(ax,ay);
         Punto2D b = new Punto2D();
+
         OperacionesConPuntos2D op = new OperacionesConPuntos2D();
-        op.metricaEuclidiana(a,b);
-        double norma = OperacionesConPuntos2D.normaEuclideana(a);
-        double dAB = OperacionesConPuntos2D.metricaEuclidiana(a,b);
-        System.out.println("La norma de " + a + " es : " + norma);
-        System.out.println("La diatancia del punto " + a +
-                            " al punto  "+ b + "  es: " + dAB);
+
+        double norma = op.normaEuclideana(a);
+        double metricaE = op.metricaEuclidiana(a,b);
+        double metricaT = op.metricaTaxista(a,b);
+        double producto = op.dotProduct(a,b);
+        double areaTriangulo = op.areaPalalelogramo(a,b);
 
 
-
+        JOptionPane.showMessageDialog(null, "La norma de " + a + " es : " + norma + "\n" +
+                "\n" + "La diatancia del punto " + a + " al punto  "+ b + "  es: " + metricaE +  "\n" +
+                "\n" + "El producto de los puntos " + a + " y " + b + " es: " + producto + "\n" +
+                "\n" + "La métrica Manhhattan de los puntos " + a + " y " + b + " es: " + metricaT + "\n" +
+                "\n" + "El área del triangulo formado entre el punto " + a + ", (" + a.getX()+ ",0.0) y " + b  + " es: "+ areaTriangulo);
 
     }
 }
